b9a4260636edb3faee10a45cec70fe6000493562
Miguel11mt <migttf_11@hotmail.com>
2017-08-12 12:59:41 -0500

    lab4
    sintasix variables

---
+++ moulo_III/lab4/index.js
@@ -0,0 +1,6 @@
+var e = {};
+var a = 'variable';
+var b = 5;
+var c = true;
+var d = ['a', 'b', 'c'];
+console.log("Variables: " + a + " " + b + " " + c + " " + d);
---
+++ moulo_III/lab4/index.ts
@@ -0,0 +1,7 @@
+let e:any={};
+let a:string='variable';
+let b:number=5;
+let c:boolean = true;
+let d:Array<string>=['a', 'b', 'c'];
+
+console.log("Variables: " + a + " "+ b+ " "+  c +" " + d);
